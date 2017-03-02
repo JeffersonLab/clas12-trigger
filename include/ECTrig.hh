cb0ba720cf3651c1dae15ee45c83d54a46e0b839
Rafayel Paremuzyan <rafopar@gmail.com>
2017-03-01 22:38:11 -0500

    Peak and cluster information now stored in structures TEC_Peak and TEC_Cluster, and these structures are stored in the class vector containers

--- include/ECTrig.hh
+++ include/ECTrig.hh
@@ -1,12 +1,35 @@
 #ifndef __ECTrig__
 #define __ECTrig__
 
- #include <evioUtil.hxx>
-// #include <evioFileChannel.hxx>
-//class evio::evioDOMNode;
+#include <evioUtil.hxx>
 #include <ap_int.h>
 #include <ap_fixed.h>
 #include <hls_stream.h>
+#include <vector>
+
+using namespace std;
+
+typedef struct{
+  int inst;
+  int view;
+  double coord;
+  int energy;
+  int time;
+} TEC_Peak;
+
+typedef struct{
+  int inst;
+  double coordY;
+  double coordX;
+  int energy;
+  int time;
+} TEC_Cluster;
+
+typedef struct{
+  int inst;
+  int lane;
+  int time;
+} TCLAS12Trigger;
 
 class TECTrig
 {
@@ -14,6 +37,22 @@ public:
   TECTrig();
   TECTrig( evio::evioDOMNode*);
   int Sector() {return fSector;};
+  bool HasBlockHeader() {return has_BlockHeader;};
+  bool HasBlockTrailer() {return has_BlockTrailer;};
+  bool HasEventHeader() {return has_EventHeader;};
+  bool HasTrigTime() {return has_TrigTime;};
+  bool HasTrigPeak() {return has_TrigPeak;};
+  bool HasTrigClust() {return has_TrigClust;};
+  bool HasTrigger() {return has_Trigger;};
+
+  int GetNwords() { return has_BlockTrailer ? fnwords: UNDEF; }
+  int GetSlotID() { return (has_BlockHeader || has_BlockTrailer) ? fslotid : UNDEF; }
+  int GetEvNumber() { return has_EventHeader ? fev_number : UNDEF; }
+  int GetBlockNUmber() {return has_BlockHeader ? fblock_number: UNDEF; }
+  int GetBlockLevel() {return has_BlockHeader ? fblock_level: UNDEF; }
+  
+  //-------  double GetPeakCoord() {return }
+  
 private:
   std::vector<ap_int<32> >::iterator fit_data;
   int fSector;
@@ -22,31 +61,24 @@ private:
   int fev_number;
   int fblock_number;
   int fblock_level;
-  
-  int fpeak_inst;
-  int fpeak_view;
-  double fpeak_coord;
-  ap_ufixed<9, 6> fpeak_coord_hls;
-  //ap_ufixed<128,32> fpeak_coord;
-  int fpeak_energy;
-  int fpeak_time;
 
-  int fclust_inst;
+  vector<TEC_Peak> fv_ECPeaks;
+  vector<TEC_Cluster> fv_ECClusters;
+
+  int fnPeaks;
+  int fnClusters;
+  
+  ap_ufixed<9, 6> fpeak_coord_hls;
+
   ap_ufixed<9, 6> fclust_coord_Y_hls;
   ap_ufixed<9, 6> fclust_coord_X_hls;
-
-  double fclust_coord_Y;
-  double fclust_coord_X;
-
-  int fclust_Energy;
-  int fclust_time;
 
   int ftrig_inst;
   int ftrig_lane;
   int ftrig_time;
-  
-  //long long int ftrig_time;
-  ap_int<48> ftrg_time;
+
+  long long int ftrg_time;
+  //ap_int<48> ftrg_time;
   ap_int<33> test_var[2];
 
   void ResetAll();
@@ -57,6 +89,14 @@ private:
   void ReadECTriggerPeak();
   void ReadECTriggerCluster();
   void ReadTrigger();
+  
+  bool has_BlockHeader;
+  bool has_BlockTrailer;
+  bool has_EventHeader;
+  bool has_TrigTime;
+  bool has_TrigPeak;
+  bool has_TrigClust;
+  bool has_Trigger;
 
   static const unsigned short int type_blk_head = 0;
   static const unsigned short int type_blk_trail = 1;
@@ -65,6 +105,8 @@ private:
   static const unsigned short int type_ECtrig_peak = 4;
   static const unsigned short int type_ECTrig_clust = 5;
   static const unsigned short int type_trigger = 6;
+
+  static const int UNDEF = -9999;
 };
 
 #endif
--- src/ECTrig.cxx
+++ src/ECTrig.cxx
@@ -18,10 +18,13 @@ TECTrig::TECTrig( evio::evioDOMNode* it ){
   
   vector<ap_int<32> > *data_values = (vector<ap_int<32>> *)it->getVector<uint32_t>();
 
+  cout<<"======******====== BEGINNIG OF VTP Hadrware Data ======******======"<<endl;
+
   for( fit_data = data_values->begin(); fit_data != data_values->end(); fit_data++ ){
 
     ap_int<1> is_type_def = fit_data->range(31, 31);
 
+    cout<<endl;    cout<<endl;
     cout<<"The word is "<<setw(15)<<(*fit_data)<<setw(36)<<bitset<32>(*fit_data)<<endl;
 
     if( is_type_def ){
@@ -47,11 +50,13 @@ TECTrig::TECTrig( evio::evioDOMNode* it ){
     }
 
   }
-  
+  cout<<"======******====== END OF VTP Hadrware Data ======******======"<<endl;
+  fnPeaks = fv_ECPeaks.size();
+  fnClusters = fv_ECClusters.size();
 }
 
 void TECTrig::ReadBlockHeader(){
-
+  has_BlockHeader = true;
   fslotid = fit_data->range(26, 22);
   fblock_number = fit_data->range(17, 8);
   fblock_level = fit_data->range(7, 0);
@@ -59,68 +64,84 @@ void TECTrig::ReadBlockHeader(){
 }
 
 void TECTrig::ReadBlockTrailer(){
+  has_BlockTrailer = true;
   fslotid = fit_data->range(26, 22);
   fnwords = fit_data->range(21, 0);
 }
 
 void TECTrig::ReadEventHeader(){
+  has_EventHeader = true;
   fev_number = fit_data->range(26, 0);
 }
 
 void TECTrig::ReadTriggerTime(){
-
+  has_TrigTime = true;
   int timel = fit_data->range(23, 0);
   cout<<"Type defining word"<<setw(15)<<(*fit_data)<<setw(36)<<bitset<32>(*fit_data)<<endl;
   fit_data = std::next(fit_data, 1);
   cout<<"Type defining word"<<setw(15)<<(*fit_data)<<setw(36)<<bitset<32>(*fit_data)<<endl;
   int timeh = fit_data->range(23, 0);
  
+  // ftrg_time = timeh;
+  // ftrg_time = ftrg_time<<24;
+  // ftrg_time = ftrg_time|timel;
+
+  ftrg_time = 0; // Make sure all bits are 0
   ftrg_time = timeh;
   ftrg_time = ftrg_time<<24;
   ftrg_time = ftrg_time|timel;
-
+ 
   cout<<"size of ftrig_time = "<<sizeof(ftrg_time)<<endl;
   cout<<"timel           "<<bitset<48>(timel)<<endl;
   cout<<"timeh           "<<bitset<48>(timeh)<<endl;
-  cout<<"ftrig_time      "<<bitset<48>(ftrg_time)<<endl;
-  cout<<"Trigger time is "<<ftrg_time<<endl;
+  cout<<"ftrig_time       "<<bitset<48>(ftrg_time)<<endl;
+  //cout<<"aftrig_time      "<<bitset<48>(aftrig_time)<<endl;
+  cout<<"Trigger time is  "<<ftrg_time<<endl;
+  //cout<<"aTrigger time is "<<aftrig_time<<endl;
 
   test_var[0] = 15; test_var[1] = 75;
   cout<<"Addr1 = "<<&test_var[0]<<"     Addr2 = "<<&test_var[1]<<endl;
-  cout<<"width of ftrig_time = "<<ftrg_time.getBitWidth()<<endl;
+  //cout<<"width of ftrig_time = "<<ftrg_time.getBitWidth()<<endl;
 }
 
 
 void TECTrig::ReadECTriggerPeak(){
+  has_TrigPeak = true;
   cout<<"  Data word is  "<<bitset<32>(fit_data->range(31, 0))<<endl;
-  fpeak_inst = fit_data->range(26, 26);
-  fpeak_view = fit_data->range(25, 24);
-
+  TEC_Peak cur_peak;
+  cur_peak.inst = fit_data->range(26, 26);
+  cur_peak.view = fit_data->range(25, 24);
   fpeak_coord_hls(8, 0) = fit_data->range(23, 15);
-  fpeak_coord = fpeak_coord_hls.to_double();
-  fpeak_energy = fit_data->range(14, 2);
+  cur_peak.coord = fpeak_coord_hls.to_double();
+  cur_peak.energy = fit_data->range(14, 2);
 
   fit_data = std::next(fit_data, 1);   // Go to the next word
   // This should be data continuation word, if not then data format is not correct
   if( fit_data->range(31, 31) ) {printf("Wrong Data Format in %s Exiting", __func__); exit(0); }
-  fpeak_time = fit_data->range(10, 0);
+  
+  cur_peak.time = fit_data->range(10, 0);
+
+  fv_ECPeaks.push_back(cur_peak);
+  
   cout<<"Data cont. word"<<setw(15)<<(*fit_data)<<setw(36)<<bitset<32>(*fit_data)<<endl;
-  cout<<"======= Inst = "<<fpeak_inst<<endl;
-  cout<<"======= view = "<<fpeak_view<<endl;
-  cout<<"======= The peak coordinate is "<<fpeak_coord<<endl;
-  cout<<"======= The peak coordinate is "<<bitset<9>(fpeak_coord)<<endl;
-  cout<<"======= Energy = "<<fpeak_energy<<endl;
-  cout<<"======= Time = "<<fpeak_time<<endl;
+  cout<<"======= Inst = "<<cur_peak.inst<<endl;
+  cout<<"======= view = "<<cur_peak.view<<endl;
+  cout<<"======= The peak coordinate is "<<cur_peak.coord<<endl;
+  cout<<"======= The peak coordinate is "<<bitset<9>(cur_peak.coord)<<endl;
+  cout<<"======= Energy = "<<cur_peak.energy<<endl;
+  cout<<"======= Time = "<<cur_peak.time<<endl;
 }
 
 void TECTrig::ReadECTriggerCluster(){
+  has_TrigClust = true;
   cout<<" === === === === ECTrigCluster === === === ===  "<<endl;
   cout<<"  Data word is  "<<bitset<32>(fit_data->range(31, 0))<<endl;
-  fclust_inst = fit_data->range(26, 26);
+  TEC_Cluster cur_clust;
+  cur_clust.inst = fit_data->range(26, 26);
   fclust_coord_Y_hls(8, 0) = fit_data->range(25, 17);
-  fclust_coord_Y = fclust_coord_Y_hls.to_double();
+  cur_clust.coordY = fclust_coord_Y_hls.to_double();
   fclust_coord_X_hls(8, 0) = fit_data->range(16, 8);
-  fclust_coord_X = fclust_coord_X_hls.to_double();
+  cur_clust.coordX = fclust_coord_X_hls.to_double();
   
   //cout<<"Type defining word"<<setw(15)<<(*fit_data)<<setw(36)<<bitset<32>(*fit_data)<<endl;
 
@@ -133,13 +154,15 @@ void TECTrig::ReadECTriggerCluster(){
 
   cout<<"Data cont. word"<<setw(15)<<(*fit_data)<<setw(36)<<bitset<32>(*fit_data)<<endl;
   
-  fclust_Energy = fit_data->range(29, 17);
-  fclust_time = fit_data->range(10, 0);
+  cur_clust.energy = fit_data->range(29, 17);
+  cur_clust.time = fit_data->range(10, 0);
+
+  fv_ECClusters.push_back(cur_clust);
   
-  cout<<"Y coordinate = "<<fclust_coord_Y<<endl;
-  cout<<"X coordinate = "<<fclust_coord_X<<endl;
-  cout<<"Cluster Energy = "<<fclust_Energy<<endl;
-  cout<<"Cluster Time = "<<fclust_time<<endl;
+  cout<<"Y coordinate = "<<cur_clust.coordY<<endl;
+  cout<<"X coordinate = "<<cur_clust.coordY<<endl;
+  cout<<"Cluster Energy = "<<cur_clust.energy<<endl;
+  cout<<"Cluster Time = "<<cur_clust.time<<endl;
   
   cout<<" === === === ===  End of ECTrigCluster === === === ===  "<<endl;
 }
@@ -147,6 +170,7 @@ void TECTrig::ReadECTriggerCluster(){
 
 
 void TECTrig::ReadTrigger(){
+  has_Trigger = true;
   cout<<" === === === === Trigger === === === ===  "<<endl;
   cout<<"  Data word is  "<<bitset<32>(fit_data->range(31, 0))<<endl;
 
@@ -162,31 +186,31 @@ void TECTrig::ReadTrigger(){
 
 
 void TECTrig::ResetAll(){
+  fSector=UNDEF;
+  fslotid=UNDEF;
+  fnwords=UNDEF;
+  fev_number=UNDEF;
+  fblock_number=UNDEF;
+  fblock_level=UNDEF;
   
-  fSector=-9999;
-  fslotid=-9999;
-  fnwords=-9999;
-  fev_number=-9999;
-  fblock_number=-9999;
-  fblock_level=-9999;
+  ftrig_inst=UNDEF;
+  ftrig_lane=UNDEF;
+  ftrig_time=UNDEF;
   
-  fpeak_inst=-9999;
-  fpeak_view=-9999;
-  fpeak_coord=-9999;
-  fpeak_energy=-9999;
-  fpeak_time=-9999;
+  ftrg_time=0;
 
-  fclust_inst=-9999;
-  fclust_coord_Y=-9999;
-  fclust_coord_X=-9999;
-
-  fclust_Energy=-9999;
-  fclust_time=-9999;
-
-  ftrig_inst=-9999;
-  ftrig_lane=-9999;
-  ftrig_time=-9999;
+  fv_ECPeaks.clear();
+  fv_ECPeaks.shrink_to_fit();
+  fv_ECClusters.clear();
+  fv_ECClusters.shrink_to_fit();
+  fnPeaks = 0;
+  fnClusters = 0;
   
-  ftrg_time=-9999;
-  
+  has_BlockHeader = false;
+  has_BlockTrailer = false;
+  has_EventHeader = false;
+  has_TrigTime = false;
+  has_TrigPeak = false;
+  has_TrigClust = false;
+  has_Trigger = false;
 }
