3bf56a9f2763e001be7e8bcda2a8113870570eab
Rafayel Paremuzyan <rafopar@jlab.org>
2017-12-17 15:14:27 -0500

    TTrigBit class is added, which encodes which trigger bits are set

---
+++ include/TTrigBit.hh
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   TTrigBit.hh
+ * Author: rafopar
+ *
+ * Created on December 15, 2017, 6:51 PM
+ */
+
+#ifndef TTRIGBIT_HH
+#define TTRIGBIT_HH
+
+#include <evioUtil.hxx>
+#include <evioFileChannel.hxx>
+
+#include <ap_int.h>
+#include <ap_fixed.h>
+#include <hls_stream.h>
+
+class TTrigBit {
+public:
+    TTrigBit(evio::evioDOMNode*, int);
+    TTrigBit(const TTrigBit& orig);
+
+    bool Main() {return ftrword.range(0, 0);};  // Return whether the main trigger is fired
+    bool El_Sec(int); // return true if the electron trigger in the sector is fired, sect should be in the (0-5) range
+    bool HTCC_Sec(int); // return true if the HTCC trigger in the sector is fired, sect should be in the (0-5) range
+    bool PCal_Sec(int); // return true if the PCal trigger in the sector is fired, sect should be in the (0-5) range
+    bool PC_Sec(int); // return true if the EC trigger in the sector is fired, sect should be in the (0-5) range
+    bool HTCCPCal() {return ftrword.range(25, 25);}; // return true if the HTCC-PCal coincidence trigger is fired
+    bool HTCCEC(){return ftrword.range(26, 26);}; // return true if the HTCC-EC coincidence trigger is fired
+    bool PCalEC(){return ftrword.range(27, 27);};; // return true if the PCal-EC coincidence trigger is fired
+    bool Pulser(){return ftrword.range(31, 31);};; // return true if the pulser trigger is fired
+    
+    void SetevioDOMENodeCrateID(evio::evioDOMNode*, int);
+    void ResetAll();
+    virtual ~TTrigBit();
+
+    ap_int<32> funprscaled_trword;
+    ap_int<32> ftrword;
+
+private:
+
+    static const int ftrig_crat_ID = 37;
+
+    std::vector<ap_int<32> >::iterator fit_data;
+
+
+};
+
+#endif /* TTRIGBIT_HH */
+
---
+++ src/TTrigBit.cxx
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   TTrigBit.cxx
+ * Author: rafopar
+ * 
+ * Created on December 15, 2017, 6:51 PM
+ */
+
+#include "TTrigBit.hh"
+
+TTrigBit::TTrigBit(evio::evioDOMNode* it, int a_crate_tag) {
+
+    SetevioDOMENodeCrateID(it, a_crate_tag);
+
+}
+
+void TTrigBit::SetevioDOMENodeCrateID(evio::evioDOMNode* it, int a_crate_tag) {
+
+    if (ftrig_crat_ID != a_crate_tag) {
+        printf("Wrong tag number in %s, tag is %d Exiting \n", __func__, a_crate_tag);
+        exit(0);
+    }
+
+    vector<ap_int<32> > *data_values = (vector<ap_int < 32 >> *)it->getVector<uint32_t>();
+
+    // THis bank should have 7 words
+
+    if (data_values->size() != 7) {
+        printf("Wrong number of words in %s, NUmber of words should be 7, but it is  %d Exiting \n", __func__, int(data_values->size()));
+        exit(0);
+    }
+
+    funprscaled_trword(31, 0) = (data_values->at(6)).range(31, 0);
+    ftrword(31, 0) = (data_values->at(4)).range(31, 0);
+
+}
+
+// return true if the electron trigger in the sector is fired, sect should be in the (0-5) range
+
+bool TTrigBit::El_Sec(int a_sec) {
+
+    if (a_sec < 0 || a_sec > 5) {
+        printf("Wrong sector is given %s, Sector should be in thr (0-5) range, but it is  %d Exiting \n", __func__, a_sec);
+        exit(0);
+    }
+    return ftrword.range(1 + a_sec, 1 + a_sec);
+}
+
+
+// return true if the HTCC trigger in the sector is fired, sect should be in the (0-5) range
+bool TTrigBit::HTCC_Sec(int a_sec) {
+
+    if (a_sec < 0 || a_sec > 5) {
+        printf("Wrong sector is given %s, Sector should be in thr (0-5) range, but it is  %d Exiting \n", __func__, a_sec);
+        exit(0);
+    }
+
+    return ftrword.range(7 + a_sec, 7 + a_sec);
+}
+
+// return true if the PCal trigger in the sector is fired, sect should be in the (0-5) range
+bool TTrigBit::PCal_Sec(int a_sec) {
+
+    if (a_sec < 0 || a_sec > 5) {
+        printf("Wrong sector is given %s, Sector should be in thr (0-5) range, but it is  %d Exiting \n", __func__, a_sec);
+        exit(0);
+    }
+
+    return ftrword.range(13 + a_sec, 13 + a_sec);
+}
+
+TTrigBit::TTrigBit(const TTrigBit& orig) {
+}
+
+TTrigBit::~TTrigBit() {
+}
+
+void TTrigBit::ResetAll() {
+    funprscaled_trword = 0;
+    ftrword = 0;
+}
